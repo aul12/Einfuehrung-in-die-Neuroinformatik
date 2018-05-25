7b45a5c35d39e30f6336eebd6b377b65916f4292
Paul Nykiel <paul.nykiel@gmail.com>
2018-05-25 12:40:43 +0200

    Matlab Müll

--- Blatt05/initWeights.m
+++ Blatt05/initWeights.m
@@ -9,8 +9,8 @@ function [weights] = initWeights(inputDimensions, hiddenNeurons, outputDimension
 %       - weights: struct with the parameters w1, w2, theta1 and theta2
 %
     rng(1337, 'combRecursive');
-    weights.w1 = rand(hiddenNeurons,inputDimensions) .- 0.5;
-    weights.w2 = rand(outputDimensions, hiddenNeurons) .- 0.5;
-    weights.theta1 = rand(hiddenNeurons,1) .-0.5;
-    weights.theta2 = rand(outputDimensions,1) .-0.5;
+    weights.w1 = rand(hiddenNeurons,inputDimensions) - 0.5;
+    weights.w2 = rand(outputDimensions, hiddenNeurons) - 0.5;
+    weights.theta1 = rand(hiddenNeurons,1) - 0.5;
+    weights.theta2 = rand(outputDimensions,1) - 0.5;
 end
